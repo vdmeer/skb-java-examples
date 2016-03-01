17e3431ee7ba7cb2344a5e7071de6ff63fbdcb16
vdmeer <sven@vandermeer.de>
2016-03-01 15:42:11 +0000

    set file versions and passed tests for version v0.0.7

--- src/main/java/de/vandermeer/skb/examples/atv2/package-info.java
+++ src/main/java/de/vandermeer/skb/examples/atv2/package-info.java
@@ -17,7 +17,7 @@
  * Examples for {@link de.vandermeer.asciitable.v2.V2_AsciiTable} implemented as a shell and CLI tools.
  *
  * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
- * @version    v0.0.7-SNAPSHOT build 160301 (01-Mar-16) for Java 1.8
+ * @version    v0.0.7 build 160301 (01-Mar-16) for Java 1.8
  * @since      v0.0.1
  */
 package de.vandermeer.skb.examples.atv2;
